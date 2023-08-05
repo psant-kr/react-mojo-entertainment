49d0ec782a319195c3977ee73e6f2d40d64ec18c
prashant kumar <prashantdoa1996@gmail.com>
2023-08-05 14:21:43 +0530

    routing completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { Container } from '@mui/material';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className="app">
-        prashant
+        <Container>
+          <Routes>
+            <Route path='/' Component={Trending} exact />
+            <Route path='/movies' Component={Movies} />
+            <Route path='/series' Component={Series} />
+            <Route path='/search' Component={Search} />
+
+          </Routes>
+        </Container>
       </div>
-      <SimpleBottomNavigation/>
-    </>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
 
   );
 }
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -6,10 +6,20 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import TvIcon from '@mui/icons-material/Tv';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (value === 0) navigate("/");
+        else if (value === 1) navigate("/movies");
+        else if (value === 2) navigate("/series");
+        else if (value === 3) navigate("/search");
+    }, [value, navigate])
+
 
     return (
         <Box sx={{
@@ -31,20 +41,20 @@ export default function SimpleBottomNavigation() {
                     label="Trending"
                     icon={<WhatshotIcon />}
                 />
-                <BottomNavigationAction 
-                style={{ color: "red" }}
-                label="Movies" 
-                icon={<MovieIcon />} 
+                <BottomNavigationAction
+                    style={{ color: "red" }}
+                    label="Movies"
+                    icon={<MovieIcon />}
                 />
-                <BottomNavigationAction 
-                style={{ color: "red" }}
-                label="TV Series" 
-                icon={<TvIcon />} 
+                <BottomNavigationAction
+                    style={{ color: "red" }}
+                    label="TV Series"
+                    icon={<TvIcon />}
                 />
-                <BottomNavigationAction 
-                style={{ color: "red" }}
-                label="Search" 
-                icon={<SearchIcon />} 
+                <BottomNavigationAction
+                    style={{ color: "red" }}
+                    label="Search"
+                    icon={<SearchIcon />}
                 />
             </BottomNavigation>
         </Box>
