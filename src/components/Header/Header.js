6b83369ca2bb6062cf20c93f88dca40affbc74c3
prashant kumar <prashantdoa1996@gmail.com>
2023-08-08 16:49:30 +0530

    smooth scroll added in trending page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <span className='header'>📹 MOJO Entertainment 🎬</span>
+    <span onClick={()=>window.scroll(0,0)} className='header'>📹 MOJO Entertainment 🎬</span>
   )
 }
 
