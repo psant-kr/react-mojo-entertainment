8dae15fd52ee06edb2c72f7f22b1681c8f24aecc
prashant kumar <prashantdoa1996@gmail.com>
2023-08-11 01:05:23 +0530

    tabs and tab added with functionality  and design

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Button, TextField, ThemeProvider, createTheme } from '@mui/material'
+import { Button, Tab, Tabs, TextField, ThemeProvider, createTheme } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 
 const Search = () => {
     const [type, setType] = useState(0);
+    const [page, setPage] = useState(1);
 
     const darkTheme = createTheme({
         palette: {
@@ -17,7 +18,7 @@ const Search = () => {
     return (
         <div>
             <ThemeProvider theme={darkTheme}>
-                <div style={{display:'flex',margin:"20px 0"}}>
+                <div style={{ display: 'flex', margin: "20px 0" }}>
                     <TextField
                         style={{ flex: 1 }}
                         className='searchBox'
@@ -32,6 +33,36 @@ const Search = () => {
                         <SearchIcon />
                     </Button>
                 </div>
+
+                <Tabs
+                    value={type}
+                    textColor="primary"
+                    indicatorColor="primary"
+                    onChange={(event, newValue) => {
+                        setPage(1);
+                        setType(newValue);
+                    }}
+                    style={{ paddingBottom: 5 }}
+                >
+                    <Tab
+                        label="Search Movies"
+                        style={{
+                            width: "50%",
+                            textTransform: "capitalize",
+                            fontSize: "16px"
+                        }}
+                    />
+                    <Tab
+                        label="Search TV Series"
+                        style={{
+                            width: "50%",
+                            textTransform: "capitalize",
+                            fontSize: "16px"
+                        }}
+                    />
+
+                </Tabs>
+
             </ThemeProvider>
         </div>
     )
