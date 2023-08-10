895fe261367e9103403efc4ea747642bc7ca6c17
prashant kumar <prashantdoa1996@gmail.com>
2023-08-11 00:16:38 +0530

    button and search icon added styling done

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { TextField, createTheme } from '@mui/material'
+import { Button, TextField, ThemeProvider, createTheme } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Search = () => {
     const [type, setType] = useState(0);
@@ -15,13 +16,23 @@ const Search = () => {
 
     return (
         <div>
-            <TextField
-                style={{ flex: 1 }}
-                className='searchBox'
-                label="Search"
-                variant="filled"
-            // onChange={(e)=>setSearchText(e.target.value)}
-            />
+            <ThemeProvider theme={darkTheme}>
+                <div style={{display:'flex',margin:"20px 0"}}>
+                    <TextField
+                        style={{ flex: 1 }}
+                        className='searchBox'
+                        label="Search"
+                        variant="filled"
+                    // onChange={(e)=>setSearchText(e.target.value)}
+                    />
+                    <Button
+                        variant="contained"
+                        style={{ marginLeft: 12 }}
+                    >
+                        <SearchIcon />
+                    </Button>
+                </div>
+            </ThemeProvider>
         </div>
     )
 }
