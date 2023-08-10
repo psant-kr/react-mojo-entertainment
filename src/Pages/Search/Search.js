da98a94b50d5e202bf89cd21e94701e3527e1bfa
prashant kumar <prashantdoa1996@gmail.com>
2023-08-11 02:19:53 +0530

    here 401 error in the web as well as console axios is giving 401 error api is not able to get hit

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Tab, Tabs, TextField, ThemeProvider, createTheme } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios';
+import CustomPagination from '../../components/Pagination/CustomPagination'
+import SingleContent from '../../components/SingleContent/SingleContent'
 
 const Search = () => {
     const [type, setType] = useState(0);
     const [page, setPage] = useState(1);
+    const [searchText, setSearchText] = useState("");
+    const [content, setContent] = useState();
+    const [numOfPages, setNumOfPages] = useState();
 
     const darkTheme = createTheme({
         palette: {
@@ -15,6 +21,25 @@ const Search = () => {
         },
     });
 
+    const fetchSearch = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}
+             ?api_key=${process.env.REACT_APP_API_KEY}
+            &language=en-US&query=${searchText}
+            &page=${page}&include_adult=false`
+        );
+        console.log(data);
+        setContent(data.results);
+        setNumOfPages(data.total_pages);
+    };
+
+    useEffect(() => {
+        window.scroll(0, 0);
+        fetchSearch();
+        // eslint-disable-next-line
+    }, [type, page]);
+
+
     return (
         <div>
             <ThemeProvider theme={darkTheme}>
@@ -24,11 +49,12 @@ const Search = () => {
                         className='searchBox'
                         label="Search"
                         variant="filled"
-                    // onChange={(e)=>setSearchText(e.target.value)}
+                        onChange={(e) => setSearchText(e.target.value)}
                     />
                     <Button
                         variant="contained"
                         style={{ marginLeft: 12 }}
+                        onClick={fetchSearch}
                     >
                         <SearchIcon />
                     </Button>
@@ -64,6 +90,34 @@ const Search = () => {
                 </Tabs>
 
             </ThemeProvider>
+
+            <div className='trending'>
+                {
+                    content && content.map((c) =>
+                        <SingleContent
+                            key={c.id}
+                            id={c.id}
+                            poster={c.poster_path}
+                            title={c.title || c.name}
+                            date={c.release_date || c.first_air_date}
+                            // media_type={c.media_type}
+                            media_type={type ? "tv" : "movie"}
+                            vote_average={c.vote_average}
+                            adult={c.adult}
+                        />
+                    )
+                }
+                {
+                    searchText &&
+                    !content &&
+                    (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)
+                }
+            </div>
+
+            {numOfPages > 1 && (
+                <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+            )}
+
         </div>
     )
 }
